a4647e6069e7380bc3394a569f587ce31a54b8a4
darjwx <darjwx@gmail.com>
2022-03-16 13:24:38 +0100

    fusion: train script: log info during training

--- train.sh
+++ train.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# $1 gpu id
+# $2 log name
 
 echo "---- Training model with gpu ${1} ----"
-CUDA_VISIBLE_DEVICES=${1} python train_model.py
+CUDA_VISIBLE_DEVICES=${1} python train_model.py 2>&1 | tee logs/log-${2}.txt
