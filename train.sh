fbbf1cdb7e4e2f38b3c3cc3ca34236e73617c120
darjwx <darjwx@gmail.com>
2022-03-30 15:18:20 +0200

    fusion: train script: make config_path configurable directly through the bash script

--- train.sh
+++ train.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # $1 gpu id
 # $2 log name
+# $3 dataset configs
 
 echo "---- Training model with gpu ${1} ----"
-CUDA_VISIBLE_DEVICES=${1} python train_model.py 2>&1 | tee logs/log-${2}.txt
+CUDA_VISIBLE_DEVICES=${1} python train_model.py --config_path ${3} 2>&1 | tee logs/log-${2}.txt
